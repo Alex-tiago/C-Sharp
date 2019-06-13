1865710f641922fe44e75e9d092e4bb21d942ac5
Alex Santiago <35665728+Alex-tiago@users.noreply.github.com>
2019-06-13 10:36:11 -0400

    Add files via upload

---
+++ prime/Prime/Prime/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prime
+{
+    class Program
+    {
+        static bool IsPrime(int num)
+        {
+            for (int i = 2; i < num; i++)
+                if (num % i == 0)
+                    return false;
+            return true;
+        }
+        public static void Main()
+        {
+
+            Console.Write("Enter a number : ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            if (IsPrime(n))
+                Console.WriteLine(n + " is a prime number");
+            else
+                Console.WriteLine(n + " is not a prime number");
+
+
+        }
+    }
+}
